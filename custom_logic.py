039aaf9119bdedaf5e2a441a05930b039650f570
Rozhdestvenskiy Dmitriy <dremsama@gmail.com>
2018-06-07 19:34:02 +0300

    sample version 0.9

--- custom_logic.py
+++ custom_logic.py
@@ -1,6 +1,8 @@
 import configuration
 import pyodbc
 import logging
+import pymsteams
+import re
 
 
 class VeeamEvent(object):
@@ -8,21 +10,57 @@ class VeeamEvent(object):
         self.raw_event_info = event_info
         self.target_notification_channel = target_notification_channel
         self.completed = False
+        self.job_name = self.raw_event_info[0][0]
+        self.job_type = int(self.raw_event_info[0][1])
+        if self.job_type == 51:
+            self.job_type_name = 'Backup copy job'
+        elif self.job_type == 0:
+            self.job_type_name = 'Backup job'
+        elif self.job_type == 1:
+            self.job_type_name = 'Replication job'
+        elif self.job_type == 3:
+            self.job_type_name = 'SureBackup job'
+        elif self.job_type == 24:
+            self.job_type_name = 'File to tape job'
+        elif self.job_type == 28:
+            self.job_type_name = 'Backup to tape job'
+        elif self.job_type == 100:
+            self.job_type_name = 'Configuration backup'
+        else:
+            self.job_type_name = None
+        # if you need, contact @Dmitry Rozhdestvenskiy at Veeam Support to get more job types
+        self.usn = self.raw_event_info[0][2]
+        self.end_time = self.raw_event_info[0][3]
+        self.result = self.raw_event_info[0][4]
+        if self.result == 0:
+            self.result_text = 'success'
+        elif self.result == 1:
+            self.result_text = 'warning'
+        elif self.result == 2:
+            self.result_text = 'failed'
+        else:
+            self.result_text = None
+        self.reason = self.raw_event_info[0][5]
 
 
 class SQLConnectorVeeamDB:
     def __init__(self, sql_config: configuration.SQLConfigVeeamDB):
-        self.connection = pyodbc.connect(
-            'DRIVER=' + sql_config.Driver + ';PORT=1433;SERVER=' + sql_config.Server + ';PORT=1443;DATABASE='
-            + sql_config.Database + ';UID=' + sql_config.Username + ';PWD=' + sql_config.Password)
-        self.cursor = self.connection.cursor()
         self.logging_inst = logging.getLogger()
-        self.logging_inst.debug('Connected to SQL Server ' + sql_config.Server + ', DB name: ' + sql_config.Database)
+        try:
+            self.connection = pyodbc.connect(
+                'DRIVER=' + sql_config.Driver + ';PORT=1433;SERVER=' + sql_config.Server + ';PORT=1443;DATABASE='
+                + sql_config.Database + ';UID=' + sql_config.Username + ';PWD=' + sql_config.Password)
+            self.cursor = self.connection.cursor()
+            self.logging_inst.debug('Connected to SQL Server ' + sql_config.Server + ', DB name: ' + sql_config.Database)
+        except Exception as error:
+            self.logging_inst.debug(
+                'Unable to connect to SQL Server ' + sql_config.Server + ', DB name: ' + sql_config.Database + ', with error: \n' + str(error))
+            exit(1)
 
-    def select_failed_job_sessions_during_latest_hour(self)->tuple:
+    def select_completed_job_sessions_during_latest_hour(self)->tuple:
         query = 'select job_name,job_type, usn, end_time, result, reason ' \
                 'from [dbo].[Backup.Model.JobSessions] ' \
-                'where state = -1 and result in (1,2) and datediff(HH,[end_time],GETDATE()) <= 1 ' \
+                'where state = -1 and result != -1 and datediff(HH,[end_time],GETDATE()) <= 1 ' \
                 'order by usn'
         self.cursor.execute(query)
         rows = self.cursor.fetchall()
@@ -53,10 +91,10 @@ class SQLConnectorVeeamDB:
             else:
                 raise ResourceWarning
 
-    def select_failed_job_sessions_after_usn(self, usn: int)->tuple:
+    def select_completed_job_sessions_after_usn(self, usn: int)->tuple:
         query = 'select job_name,job_type, usn, end_time, result, reason ' \
                 'from [dbo].[Backup.Model.JobSessions] ' \
-                'where state = -1 and result in (1,2) and usn > ? ' \
+                'where state = -1 and result != -1 and usn > ? ' \
                 'order by usn'
         self.cursor.execute(query, usn)
         rows = self.cursor.fetchall()
@@ -86,3 +124,62 @@ class SQLConnectorVeeamDB:
                 return result
             else:
                 raise ResourceWarning
+
+
+def send_notification_to_web_hook(web_hook_url: str, event_object: VeeamEvent)->bool:
+    logger_inst = logging.getLogger()
+    logger_inst.debug('web_hook_url: ' + str(web_hook_url))
+    logger_inst.debug('threat: ' + str(event_object))
+    if uri_validator(web_hook_url) is not True:
+        logger_inst.error('Malformed url: ' + str(web_hook_url))
+        return False
+    team_connection = pymsteams.connectorcard(web_hook_url)
+    text = None
+    if event_object.job_type_name is not None:
+        if event_object.result_text == 'success':
+            text = 'A Veeam ' + event_object.job_type_name + ' **"' + str(event_object.job_name) + '"** has finished **successfully** at ' + str(event_object.end_time)[:-7]
+            team_connection.color('005f4b')  # it's a brand color named "Veeam Sapphire", btw
+        elif event_object.result_text == 'warning':
+            text = 'A Veeam ' + event_object.job_type_name + ' **"' + str(event_object.job_name) + '"** has finished with **warning** and result: \n\n"' + str(event_object.reason) + '" at ' + str(event_object.end_time)[:-7]
+            team_connection.color('ffff00')  # just Yellow
+        elif event_object.result_text == 'failed':
+            text = 'A Veeam ' + event_object.job_type_name + ' **"' + str(event_object.job_name) + '"** has **failed** with a result: \n\n"' + str(event_object.reason) + '" at ' + str(event_object.end_time)[:-7]
+            team_connection.color('ba0200')  #  "Veeam Accent Red"
+    else:
+        if event_object.result_text == 'success':
+            # There is no need to notify about system events when everything is good
+            return True
+        elif event_object.result_text == 'warning':
+            text = 'A Veeam Task **"' + event_object.job_name + '"** has finished with **warning** and result: \n\n"' + str(event_object.reason).replace('\n\n', ' ') + '" at ' + str(event_object.end_time)[:-7]
+            team_connection.color('ffff00')  # just Yellow
+        elif event_object.result_text == 'failed':
+            text = 'A Veeam Task **"' + event_object.job_name + '"** has **failed** with a result: \n\n"' + str(event_object.reason).replace('\n\n', ' ') + '" at ' + str(event_object.end_time)[:-7]
+            team_connection.color('ba0200')  # "Veeam Accent Red"
+    if text is None:
+        return False
+    team_connection.text(text)
+    try:
+        result = team_connection.send()
+    except Exception as error:
+        logger_inst.error('Unable to send notification to MS Teams due to the following error: \n' + str(error))
+        return False
+    return result
+
+
+def uri_validator(ulr) -> bool:
+    regex = re.compile(
+        r'^(?:http|ftp)s?://'  # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+        r'localhost|'  # localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|'  # ...or ipv4
+        r'\[?[A-F0-9]*:[A-F0-9:]+\]?)'  # ...or ipv6
+        r'(?::\d+)?'  # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    try:
+        result = regex.match(ulr)
+        if result is not None:
+            return True
+        else:
+            return False
+    except:
+        return False
--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from configuration import SQLConfigVeeamDB, Integration, TeamsChannels
 from custom_logic import SQLConnectorVeeamDB, VeeamEvent
-import logging
-from datetime import datetime
-import sys
+import custom_logic
+import configparser
 import time as other_time
 from logger_init import logging_config
 
-
 ConfigurationInstance = Integration()
+MainLogger = logging_config(integration_config=ConfigurationInstance, logging_mode='DEBUG', log_to_file=False, executable_path=__file__)
 SqlConfigInstanceVeeamDB = SQLConfigVeeamDB()
 SqlConnectorInstanceVeeamDB = SQLConnectorVeeamDB(SqlConfigInstanceVeeamDB)
-MainLogger = logging_config(integration_config=ConfigurationInstance, logging_mode='INFO', log_to_file=False, executable_path=__file__)
 MainLogger.info('Main process has been initialized')
+IniFile = configparser.ConfigParser()
+IniFileName = 'configuration.ini'
 
 
 def main_execution(latest_usn_func: int = 0, query_delay: int = 60):
@@ -19,10 +19,9 @@ def main_execution(latest_usn_func: int = 0, query_delay: int = 60):
     ms_teams_inst = TeamsChannels()
     # loading latest events from Veeam DataBase
     if latest_usn_func == 0:
-        latest_usn_func, raw_event_list = SqlConnectorInstanceVeeamDB.select_failed_job_sessions_during_latest_hour()
+        latest_usn_func, raw_event_list = SqlConnectorInstanceVeeamDB.select_completed_job_sessions_during_latest_hour()
     else:
-        latest_usn_func, raw_event_list = SqlConnectorInstanceVeeamDB.select_failed_job_sessions_after_usn(latest_usn_func)
-    print(latest_usn_func, raw_event_list)
+        latest_usn_func, raw_event_list = SqlConnectorInstanceVeeamDB.select_completed_job_sessions_after_usn(latest_usn_func)
     # parsing them and posting to MS Teams
     if len(raw_event_list) > 0:
         events_to_process = []
@@ -32,21 +31,44 @@ def main_execution(latest_usn_func: int = 0, query_delay: int = 60):
             events_to_process.append(verified_event)
         for each_verified_event in events_to_process:
             if type(each_verified_event) == VeeamEvent:
-                print(each_verified_event.completed)
+                result = custom_logic.send_notification_to_web_hook(event_object=each_verified_event, web_hook_url=each_verified_event.target_notification_channel)
+                if result is True:
+                    MainLogger.info('Veeam event ' + str(each_verified_event.job_name) + ' created at ' + str(each_verified_event.end_time) + ' is processed')
+                    # save its each_verified_event.usn to a text ini to use later
+
+                    each_verified_event.completed = True
+
+                else:
+                    MainLogger.error('The event ' + str(each_verified_event.job_name) + ' created at' + str(
+                        each_verified_event.end_time) + ' is NOT processed, some error occurred')
+                    # here you can do smth with such events, but I don't care :)
+                    pass
             else:
                 MainLogger.info('Such event type isn\'t supported by this tinny app, '
                                 'what it is doing in events_to_process? :)')
             MainLogger.debug(str(type(each_verified_event)))
     
 
-    # waiting query_delay, default: 60 sec
+    # waiting a query_delay, default: 60 sec
     other_time.sleep(query_delay)
     return latest_usn_func
 
 try:
-    latest_usn = 0
+    IniFile.read(IniFileName)
+    try:
+        latest_usn = int(IniFile['DEFAULT']['latest_usn'])
+    except KeyError:
+        latest_usn = 0
+        IniFile['DEFAULT']['latest_usn'] = str(0)
+        with open(IniFileName, 'w') as configfile:
+            IniFile.write(configfile)
     while True:
         latest_usn = main_execution(latest_usn_func=latest_usn)
+        MainLogger.debug('main_execution is done, updating ini file with a new latest_usn:' + str(latest_usn))
+        IniFile['DEFAULT']['latest_usn'] = str(latest_usn)
+        with open(IniFileName, 'w') as configfile:
+            IniFile.write(configfile)
+            MainLogger.debug('USN updated')
 
 except Exception as any_error_which_could_occur:
     MainLogger.error('Main Execution was stopped due to the following error: \n'
